6cf169b26ba475d84b466c0710f0ffd74f8763dc
M Sanjay <msnjy12o52o@gmail.com>
2023-06-12 11:30:38 +0530

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+function reverseString(str) {
+    let reversedString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    return reversedString;
+}
 
-};
+console.log(reverseString("Sanjay"));
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
