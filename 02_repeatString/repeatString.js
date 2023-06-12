be5034b2454347f7b80131501e6dbb54104f6220
M Sanjay <msnjy12o52o@gmail.com>
2023-06-12 11:17:09 +0530

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = string;
+    let repeatCount = num;
+    let repeatedString = "";
 
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        repeatedString = str.repeat(num);
+    } 
+    
+    return repeatedString;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
